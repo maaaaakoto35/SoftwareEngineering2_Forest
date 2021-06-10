47a69d4ae51657fe8d612669eda12bd2d4f4c3b0
maaaaakoto35 <maaaaakoto35@gmail.com>
2021-06-10 20:28:19 +0900

    modifies details

--- forest/Const.java
+++
@@ -1,9 +0,0 @@
-package forest;
-
-public class Const {
-    public static final int VERTICAL_GAP = 2;
-    public static final int HORIZONTAL_GAP = 25;
-    
-    public static final int WINDOW_WIDTH = 800;
-    public static final int WINDOW_HEIGHT = 800;
-}
